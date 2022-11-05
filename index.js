96932a790eb6d85001cc9d2dd50db09f3f1a9628
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-11-05 17:33:12 +0600

    update code

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@hero-one.z3ku6ig.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+async function run() {
+  try {
+    const porductCollection = client.db("emajon").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const page = req.query.page;
+      const size = req.query.size;
+      console.log(page, size);
+      const query = {};
+      const coursor = porductCollection.find(query);
+      const products = await coursor.toArray();
+      const count = await porductCollection.estimatedDocumentCount();
+      res.send({ count, products });
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => {
+  console.log(error);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port:${port} `);
+});
